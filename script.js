103cf515cc0e20b388a97654654daf270d8b0c92
patedemorgana <gabriel.volpate@gmail.com>
2022-10-24 22:37:55 -0300

    sdk :wq

---
+++ script.js
@@ -0,0 +1,11 @@
+document
+    .querySelector(".oi-sogra")
+    .addEventListener("click", function() {
+        alert("obrigado por escolher a resposta certa, levarei ela pra passear em breve")
+    })
+
+document
+    .querySelector(".oi-sogra2")
+    .addEventListener("click", function() {
+        alert("Resposta errada")
+    })
